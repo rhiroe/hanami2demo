181190f6ba543d5b81fac0e4acf7a6fa7ffe8c56
rhiroe <ride.poke@gmail.com>
2023-03-20 14:42:22 +0900

    .gitignore用意

--- backend/config/settings.rb
+++ backend/config/settings.rb
@@ -5,5 +5,8 @@ module Backend
     # Define your app settings here, for example:
     #
     # setting :my_flag, default: false, constructor: Types::Params::Bool
+    setting :database_url, constructor: Types::String
+    setting :database_user, constructor: Types::String
+    setting :database_password, constructor: Types::String
   end
 end
